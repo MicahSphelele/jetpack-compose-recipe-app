49e49b278acc5d9411d15c42b71d918b9323f239
Sphelele <sphemicah@gmail.com>
2021-05-24 15:17:46 +0200

    - Updated Theme.kt ☢️

--- app/src/main/java/com/recipeapp/presentation/theme/Theme.kt
+++ app/src/main/java/com/recipeapp/presentation/theme/Theme.kt
@@ -1,2 +1,47 @@
 package com.recipeapp.presentation.theme
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightThemeColors = lightColors(
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Black2,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
+    onSecondary = Color.Black,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = Grey1,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Black2,
+)
+
+private val DarkThemeColors = darkColors(
+    primary = Blue700,
+    primaryVariant = Color.White,
+    onPrimary = Color.White,
+    secondary = Black1,
+    onSecondary = Color.White,
+    error = RedErrorLight,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = Black1,
+    onSurface = Color.White,
+)
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean,
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkThemeColors else LightThemeColors,
+    ){
+        content()
+    }
+}
