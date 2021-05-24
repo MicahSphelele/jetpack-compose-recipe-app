558749a5ceddf6f913c5968fef1e2857c80d3de9
Sphelele <sphemicah@gmail.com>
2021-05-24 16:44:22 +0200

    - Added fonts to theme 📜

---
+++ app/src/main/java/com/recipeapp/presentation/theme/Type.kt
@@ -0,0 +1,38 @@
+package com.recipeapp.presentation.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.font
+import androidx.compose.ui.text.font.fontFamily
+import androidx.compose.ui.unit.sp
+import com.recipeapp.R
+
+val Lato = fontFamily(
+    font(R.font.lato_light, FontWeight.W300),
+    font(R.font.lato_regular, FontWeight.W400),
+    font(R.font.lato_black, FontWeight.W500),
+    font(R.font.lato_bold, FontWeight.W600)
+)
+
+val LatoTypography = Typography(
+    h1 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W500, fontSize = 30.sp),
+    h2 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W500, fontSize = 24.sp),
+    h3 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W500, fontSize = 20.sp),
+    h4 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W400, fontSize = 18.sp),
+    h5 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W400, fontSize = 16.sp),
+    h6 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W400, fontSize = 14.sp),
+    subtitle1 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W400, fontSize = 16.sp),
+    subtitle2 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W400, fontSize = 14.sp),
+    body1 = TextStyle(fontFamily = Lato, fontWeight = FontWeight.Normal, fontSize = 16.sp),
+    body2 = TextStyle(fontFamily = Lato, fontSize = 14.sp),
+    button = TextStyle(
+        fontFamily = Lato,
+        fontWeight = FontWeight.W400,
+        color = Color.White,
+        fontSize = 15.sp
+    ),
+    caption = TextStyle(fontFamily = Lato, fontWeight = FontWeight.Normal, fontSize = 12.sp),
+    overline = TextStyle(fontFamily = Lato, fontWeight = FontWeight.W400, fontSize = 12.sp)
+)
