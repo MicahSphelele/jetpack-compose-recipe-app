a278e8645f03e51751c147f565763e8b04a87496
Sphelele <sphemicah@gmail.com>
2021-05-25 17:50:30 +0200

    - Added snackbar ❤️

--- app/src/main/java/com/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -5,24 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.recipeapp.presentation.BaseApp
 import com.recipeapp.presentation.components.*
 import com.recipeapp.presentation.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -33,6 +36,7 @@ class RecipeListFragment : Fragment() {
     @Inject
     lateinit var application: BaseApp
 
+    @ExperimentalMaterialApi
     @ExperimentalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,22 +47,38 @@ class RecipeListFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
 
-                AppTheme(darkTheme = application.isDarkTheme.value) {
+                val snackbarHostState = remember { SnackbarHostState() }
 
-                    ViewRecipeList()
+                Column {
+                    Button(onClick = {
+                        lifecycleScope.launch {
+                            snackbarHostState.showSnackbar(message = "Hey look a snackbar", actionLabel = "Hide", duration = SnackbarDuration.Short)
+                        }
 
-                    val errorState = viewModel.errorState.value
-
-                    if (errorState.hasError) {
-                        AppAlertDialog(
-                            activity = requireActivity(),
-                            title = "Network Error",
-                            message = "Something went wrong : ${errorState.errorMessage}",
-                            buttonText = "Ok",
-                            state = mutableStateOf(true)
-                        )
+                    }) {
+                        Text(text = "Click Me")
                     }
+                    
+                    DecoupledSnackbar(snackbarHostState = snackbarHostState)
                 }
+                
+
+//                AppTheme(darkTheme = application.isDarkTheme.value) {
+//
+//                    ViewRecipeList()
+//
+//                    val errorState = viewModel.errorState.value
+//
+//                    if (errorState.hasError) {
+//                        AppAlertDialog(
+//                            activity = requireActivity(),
+//                            title = "Network Error",
+//                            message = "Something went wrong : ${errorState.errorMessage}",
+//                            buttonText = "Ok",
+//                            state = mutableStateOf(true)
+//                        )
+//                    }
+//                }
             }
         }
     }
@@ -110,6 +130,53 @@ class RecipeListFragment : Fragment() {
 
                 CircularIndeterminateProgressBar(isDisplayed = loading)
             }
+        }
+    }
+
+    @Composable
+    private fun SnackBarDemo(isShowing: Boolean,onHideSnackbar: () -> Unit) {
+        if (isShowing) {
+            ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+                val snackbar = createRef()
+
+                Snackbar(modifier = Modifier.constrainAs(snackbar) {
+                    this.bottom.linkTo(parent.bottom)
+                    this.start.linkTo(parent.start)
+                    this.end.linkTo(parent.end)
+                }, action = {
+                    Text(text = "Hide",
+                        modifier = Modifier.clickable(onClick = onHideSnackbar),
+                        style = MaterialTheme.typography.h5)
+                }) {
+                    Text(text = "Hey look a snackbar")
+                }
+            }
+        }
+    }
+
+    @ExperimentalMaterialApi
+    @Composable
+    private fun DecoupledSnackbar(snackbarHostState: SnackbarHostState) {
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val snackbar = createRef()
+
+            SnackbarHost(modifier = Modifier.constrainAs(snackbar) {
+                this.bottom.linkTo(parent.bottom)
+                this.start.linkTo(parent.start)
+                this.end.linkTo(parent.end)
+            } ,hostState = snackbarHostState,
+                snackbar = {
+             
+                    Snackbar(action = {
+                        TextButton(onClick = {
+                            snackbarHostState.currentSnackbarData?.dismiss()
+                        }) {
+                            Text(text = "Hide", style = TextStyle(color = Color.White))
+                        }
+                    }) {
+                        Text(text = "Hey look a snackbar")
+                    }
+                })
         }
     }
 }
