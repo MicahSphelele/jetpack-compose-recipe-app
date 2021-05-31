c4038800a4b21537066d3d75cfa34ad593a530e5
Sphelele <59883549+sphelele-ikhokha@users.noreply.github.com>
2021-05-31 11:53:56 +0200

    - Created AppLogger.kt 📜

---
+++ app/src/main/java/com/recipeapp/util/AppLogger.kt
@@ -0,0 +1,24 @@
+package com.recipeapp.util
+
+import android.util.Log
+
+object AppLogger {
+
+    private const val TAG = "RecipeApp"
+
+    fun info(message: String) {
+        Log.i(TAG, message)
+    }
+
+    fun warn(message: String) {
+        Log.w(TAG, message)
+    }
+
+    fun error(message: String) {
+        Log.e(TAG, message)
+    }
+
+    fun error(throwable: Throwable?) {
+        Log.e(TAG, "Unknown error", throwable)
+    }
+}
