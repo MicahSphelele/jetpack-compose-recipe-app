04b68842322f76934ecf6f075365071b96bb90fe
Sphelele <sphemicah@gmail.com>
2021-06-01 11:44:18 +0200

    - Created RecipeList component 💻

---
+++ app/src/main/java/com/recipeapp/presentation/components/RecipeList.kt
@@ -0,0 +1,59 @@
+package com.recipeapp.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.recipeapp.domain.model.Recipe
+import com.recipeapp.presentation.ui.recipe_list.RecipeListEvent
+import com.recipeapp.presentation.ui.recipe_list.RecipeListViewModel
+
+@ExperimentalMaterialApi
+@Composable
+fun RecipeList(loading: Boolean,
+               recipes: List<Recipe>,
+               onChangeRecipeListScrollPosition: (Int) -> Unit,
+               onTriggeredEvent: (RecipeListEvent) -> Unit,
+               page: Int, scaffoldState: ScaffoldState) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.surface)
+    ) {
+        if (loading && recipes.isEmpty()) {
+
+            LoadingRecipeListShimmer(cardHeight = 250.dp)
+
+        } else {
+            LazyColumn {
+                itemsIndexed(items = recipes) { index, recipe ->
+                    onChangeRecipeListScrollPosition(index)
+                    if ((index + 1) >= (page * RecipeListViewModel.PAGE_SIZE) && !loading) {
+                        onTriggeredEvent(RecipeListEvent.NextPageEvent)
+                    }
+                    RecipeCard(recipe = recipe, onClick = {
+
+                    })
+                }
+            }
+        }
+
+        CircularIndeterminateProgressBar(isDisplayed = loading)
+        DefaultSnackbar(
+            snackbarHostSate = scaffoldState.snackbarHostState,
+            onDismiss = {
+                scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+            },
+            modifier = Modifier.align(Alignment.BottomCenter))
+    }
+}
