55378edf7689e56d7c8f90dd0c7c1b9679b944af
Sphelele <sphemicah@gmail.com>
2021-06-03 11:18:59 +0200

    - Cleared unused import 🛢

--- app/src/main/java/com/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
