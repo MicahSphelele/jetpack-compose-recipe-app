a7e7eb442371e3813d3c5c657e969a49eae4b8db
Sphelele <sphemicah@gmail.com>
2021-06-07 18:09:57 +0200

    - Updated AppConstants 💻

--- app/src/main/java/com/recipeapp/util/AppConstants.kt
+++ app/src/main/java/com/recipeapp/util/AppConstants.kt
@@ -2,6 +2,7 @@ package com.recipeapp.util
 
 object AppConstants {
     const val IS_DARK_MODE= "isDarkMode"
+    const val UI_MODE= "uiModeState"
     const val KEY_RECIPE_ID = "recipeID"
     const val KEY_RECIPE = "recipe"
 }
