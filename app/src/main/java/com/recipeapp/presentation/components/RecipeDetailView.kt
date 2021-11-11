dfcf581e471c54adfccfa92fd156afab4a54cd34
Sphelele <sphemicah@gmail.com>
2021-11-11 08:09:30 +0200

    - Used foreach loop  💻  📦

--- app/src/main/java/com/recipeapp/presentation/components/RecipeDetailView.kt
+++ app/src/main/java/com/recipeapp/presentation/components/RecipeDetailView.kt
@@ -5,14 +5,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
@@ -92,7 +89,7 @@ fun RecipeDetailView(recipe: Recipe) {
                         )
                     }
 
-                    for (ingredient in recipe.ingredients) {
+                    recipe.ingredients.forEach { ingredient ->
                         Text(
                             text = ingredient,
                             modifier = Modifier
