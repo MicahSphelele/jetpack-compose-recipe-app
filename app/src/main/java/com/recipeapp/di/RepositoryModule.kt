f16ce954297fc4cbbfe10be18ffab65e67936da8
Sphelele <sphemicah@gmail.com>
2021-11-15 09:30:44 +0200

    - Injected  ThemeRepository  💻 💉

--- app/src/main/java/com/recipeapp/di/RepositoryModule.kt
+++ app/src/main/java/com/recipeapp/di/RepositoryModule.kt
@@ -1,13 +1,18 @@
 package com.recipeapp.di
 
+import android.content.Context
 import com.recipeapp.data.network.api.RecipeService
 import com.recipeapp.data.repository.RecipeRepositoryImpl
+import com.recipeapp.data.repository.ThemeRepositoryImpl
 import com.recipeapp.domain.repository.RecipeRepository
+import com.recipeapp.domain.repository.ThemeRepository
 import com.recipeapp.domain.util.RecipeDtoMapper
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -20,4 +25,11 @@ object RepositoryModule {
         recipeService: RecipeService,
         mapper: RecipeDtoMapper
     ): RecipeRepository = RecipeRepositoryImpl(recipeService, mapper)
+
+    @Singleton
+    @Provides
+    fun providesThemeRepository(
+        @ApplicationContext context: Context,
+        scope: CoroutineScope
+    ): ThemeRepository = ThemeRepositoryImpl(context, scope)
 }
