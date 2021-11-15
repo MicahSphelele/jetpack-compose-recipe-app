2ceca62694d39f0e558377f3ccb77d1bb1af2bb0
Sphelele <sphemicah@gmail.com>
2021-11-15 07:35:42 +0200

    - Injected GetRecipeListUseCase in UseCaseModule  💉

--- app/src/main/java/com/recipeapp/di/UseCaseModule.kt
+++ app/src/main/java/com/recipeapp/di/UseCaseModule.kt
@@ -1,11 +1,20 @@
 package com.recipeapp.di
 
+import com.recipeapp.domain.repository.RecipeRepository
+import com.recipeapp.domain.usecases.GetRecipeListUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object UseCaseModule {
 
+    @Singleton
+    @Provides
+    fun providesGetRecipeListUseCase(
+        repository: RecipeRepository
+    ): GetRecipeListUseCase = GetRecipeListUseCase(repository = repository)
 }
